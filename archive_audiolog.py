d6ae1a319552724b978da5b3a529a2fcc8a3f580
Aaron Chantrill <aaron.chantrill@dottywood.org>
2021-03-26 16:51:00 -0400

    Fixing archive file download
    Moved the archive file location into the audiolog directory.
    
    Fixed the linking to download the archive file.

--- archive_audiolog.py
+++ archive_audiolog.py
@@ -10,15 +10,20 @@ class ArchiveAudiologPlugin(plugin.STTTrainerPlugin):
         response = []
         try:
             audiolog_dir = paths.sub("audiolog")
-            archive_filename = os.path.expanduser("~/audiolog.tgz")
+            archive_filename = os.path.join(audiolog_dir, "audiolog.tgz")
             archive_file = tarfile.open(archive_filename, mode="w|gz")
-            archive_file.add(os.path.join(audiolog_dir,"audiolog.db"), arcname="audiolog_temp.db")
             wavfiles = glob.glob(os.path.join(audiolog_dir,"*.wav"))
             for wavfile in wavfiles:
                 print(wavfile)
                 archive_file.add(wavfile, arcname=os.path.basename(wavfile))
+            print(os.path.join(audiolog_dir, "audiolog.db"))
+            archive_file.add(os.path.join(audiolog_dir,"audiolog.db"), arcname="audiolog_temp.db")
             archive_file.close()
-            response.append("Created archive at {}".format(archive_filename))
+            response.append(
+                '<a href="?archive=audiolog.tgz">Created archive at {}</a>'.format(
+                    archive_filename
+                )
+            )
         except Exception as e:
             continue_next = False
             message = "Unknown"
